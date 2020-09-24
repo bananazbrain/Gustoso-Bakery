d9e9cdac285f4dc3e441ff2be03b19392435b711
bananazbrain <jv@bananazbrain.ru>
2020-09-24 19:13:54 +0300

    Fix: naming menu & add: slider in menu

---
+++ src/scripts/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	var menu = {
+		list: $('.menu__list'),
+		nav : $('.menu__nav')
+	}
+
+	menu.list.slick({
+		fade: true,
+		asNavFor: menu.nav,
+		arrows: false
+	});
+
+	menu.nav.slick({
+		slidesToShow: 3,
+		asNavFor: menu.list,
+		focusOnSelect: true,
+		variableWidth: true
+	});
+});
